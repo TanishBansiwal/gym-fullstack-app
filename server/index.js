b1cdcbe19b81ece42c448c8e59d435956adaeaec
Tanish Bansiwal <bansiwaltanish@gmail.com>
2026-04-27 16:20:36 +0530

    Updated port for deployment

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://myAtlasDBUser:<db_password>@myatlasclusteredu.hquwk.mongodb.net/?appName=myAtlasClusterEDU")
+mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/gymDB")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
@@ -52,6 +52,8 @@ app.put("/contacts/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
