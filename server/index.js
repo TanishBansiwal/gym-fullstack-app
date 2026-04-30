fec2967d7bbd4d2be0b82e312598e7a173cb19df
Tanish Bansiwal <bansiwaltanish@gmail.com>
2026-04-30 15:52:48 +0530

    Fix email transporter

--- server/index.js
+++ server/index.js
@@ -32,9 +32,7 @@ const ADMIN = {
 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
